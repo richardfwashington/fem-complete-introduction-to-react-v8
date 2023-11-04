847194612dcc02633b6fa8064e550ec29003112f
Richard Washington <richard.washington@taxassist.co.uk>
2023-11-04 15:17:36 +0000

    :bug: Fix component by returning

--- src/App.jsx
+++ src/App.jsx
@@ -3,26 +3,16 @@ import { createRoot } from "react-dom";
 import Pet from "./Pet";
 
 const App = () => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, "Adopt me!"),
-    React.createElement(Pet, {
-      name: "Luna",
-      animal: "Dog",
-      breed: "Havanese",
-    }),
-    React.createElement(Pet, {
-      name: "Clyde",
-      animal: "Tortoise",
-      breed: "Hermans",
-    }),
-    React.createElement(Pet, {
-      name: "Bagel",
-      animal: "Cat",
-      breed: "Tortoise shell",
-    }),
-  ]);
+  return (
+    <div>
+      <h1>Adopt Me!</h1>
+      <Pet name="Luna" animal="Dog" breed="Havanese" />
+      <Pet name="Clyde" animal="Tortoise" breed="Hermans" />
+      <Pet name="Bagel" animal="Cat" breed="Tortoise shell" />
+    </div>
+  );
 };
 
 const container = document.getElementById("root");
 const root = createRoot(container);
-root.render(React.createElement(App));
+root.render(<App />);
