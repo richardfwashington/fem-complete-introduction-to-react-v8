870f14cadfdd350d7c888b7aaf5fbe6d069c822c
Richard Washington <richard.washington@taxassist.co.uk>
2023-11-04 12:28:27 +0000

    :sparkles: Initialize npm, setup prettier, ESLint and add a gitignore

--- src/App.js
+++ src/App.js
@@ -1,34 +1,30 @@
 const Pet = (props) => {
-    return React.createElement("div", {}, [
-        React.createElement("h1", {}, props.name),
-        React.createElement("h2", {}, props.animal),
-        React.createElement("h2", {}, props.breed),
-    ]);
-}
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, props.name),
+    React.createElement("h2", {}, props.animal),
+    React.createElement("h2", {}, props.breed),
+  ]);
+};
 
 const App = () => {
-    return React.createElement(
-        "div",
-        {},
-        [
-            React.createElement("h1", {}, "Adopt me!"),
-            React.createElement(Pet, {
-                name: "Luna", 
-                animal: "Dog", 
-                breed: "Havanese"
-            }),
-            React.createElement(Pet, {
-                name: "Clyde", 
-                animal: "Tortoise", 
-                breed: "Hermans"
-            }),
-            React.createElement(Pet, {
-                name: "Bagel", 
-                animal: "Cat", 
-                breed: "Tortoise shell"
-            }),
-        ]
-    )            
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "Adopt me!"),
+    React.createElement(Pet, {
+      name: "Luna",
+      animal: "Dog",
+      breed: "Havanese",
+    }),
+    React.createElement(Pet, {
+      name: "Clyde",
+      animal: "Tortoise",
+      breed: "Hermans",
+    }),
+    React.createElement(Pet, {
+      name: "Bagel",
+      animal: "Cat",
+      breed: "Tortoise shell",
+    }),
+  ]);
 };
 
 const container = document.getElementById("root");
