460e371848d679ba8e31b5b7c4a14c779b9445d5
Richard Washington <richard.washington@taxassist.co.uk>
2023-11-04 11:19:11 +0000

    :sparkles: Add Pet component

--- src/App.js
+++ src/App.js
@@ -1,3 +1,11 @@
+const Pet = () => {
+    return Reac.createElement("div", {}, [
+        React.createElement("h1", {}, "Luna"),
+        React.createElement("h2", {}, "Dog"),
+        React.createElement("h2", {}, "Havanese"),
+    ]);
+}
+
 const App = () => {
     return React.createElement(
         "div",
