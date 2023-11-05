78e9e859bb6b4add4cd4e04473e067093e390b63
Richard Washington <richard.washington@taxassist.co.uk>
2023-11-05 12:37:46 +0000

    :sparkles: Add dropdown for breeds which is disabled is empty

--- src/SearchParams.jsx
+++ src/SearchParams.jsx
@@ -1,7 +1,13 @@
 import { useState } from "react";
 
+const ANIMALS = ["birds", "cat", "dog", "rabbit", "reptile"];
+
 const SearchParams = () => {
   const [location, setLocation] = useState("Seattle, WA");
+  const [animal, setAnimal] = useState("");
+  const [breed, setBreed] = useState("");
+
+  const breeds = [];
 
   return (
     <div className="search-params">
@@ -14,6 +20,35 @@ const SearchParams = () => {
             value={location}
             placeholder="Location"
           />
+        </label>
+        <label htmlFor="animal">
+          <select
+            id="animal"
+            value={animal}
+            onChange={(e) => {
+              setAnimal(e.target.value);
+            }}
+          >
+            <option />
+            {ANIMALS.map((animal) => (
+              <option key={animal}>{animal}</option>
+            ))}
+          </select>
+        </label>
+        <label htmlFor="breed">
+          <select
+            id="breed"
+            disabled={breeds.length === 0}
+            value={breed}
+            onChange={(e) => {
+              setBreed(e.target.value);
+            }}
+          >
+            <option />
+            {breeds.map((breed) => (
+              <option key={breed}>{breed}</option>
+            ))}
+          </select>
         </label>
         <button>Submit</button>
       </form>
