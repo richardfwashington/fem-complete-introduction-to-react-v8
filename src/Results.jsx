4979a4fc85457e1fff78fabf5988b36e4b8ef2e9
Richard Washington <richard.washington@taxassist.co.uk>
2023-11-05 18:40:17 +0000

    :art: Move pets list to its own component Pets

---
+++ src/Results.jsx
@@ -0,0 +1,23 @@
+import Pet from "./Pet";
+
+const Results = ({ pets }) => {
+  return (
+    <div className="search">
+      {!pets.length ? (
+        <h1>No Pets Found</h1>
+      ) : (
+        pets.map((pet) => (
+          <Pet
+            name={pet.name}
+            animal={pet.animal}
+            breed={pet.breed}
+            key={pet.id}
+          />
+          // Could also use the spread operator to ...pet to set all properties
+        ))
+      )}
+    </div>
+  );
+};
+
+export default Results;
