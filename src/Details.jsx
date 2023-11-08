47234416750f95f6678e26bd7119b7439a4edfa4
Richard Washington <richard.washington@taxassist.co.uk>
2023-11-08 16:28:40 +0000

    :sparkles: Add back button to details component

--- src/Details.jsx
+++ src/Details.jsx
@@ -1,5 +1,13 @@
+import { Link } from "react-router-dom";
+
 const Details = () => {
-  return <h2>Hi!</h2>;
+  return (
+    <div>
+      <h2>Hi!</h2>
+
+      <Link to="/">Back</Link>
+    </div>
+  );
 };
 
 export default Details;
